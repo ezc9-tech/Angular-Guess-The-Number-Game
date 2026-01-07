54674f101c259c257659a985c55ff55e63d60b3c
ezc9-tech <ezc9@outlook.com>
2026-01-06 18:02:53 -0600

    This simplified the logic for the game as well as added more checks

--- GUESS-THE-NUMBER/src/app/guess-game/guess-game.ts
+++ GUESS-THE-NUMBER/src/app/guess-game/guess-game.ts
@@ -9,26 +9,61 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './guess-game.scss',
 })
 export class GuessGame {
-  userGuess: number = 0;
+  guessedNumber ?: number;
   feedbackMessage: string = ''
-  secretNumber: number = Math.floor(Math.random() * 100) + 1;
-  numberOfAttempts: number = 10;
+  attemptsLeft: number = 10;
+  gameOver = false;
+  secretNumber = this.generateRandomNumber();
+
+  private static readonly MAZ_ATTEMPTS = 10;
+  private static readonly MAX_NUMBER = 100;
+
+  private generateRandomNumber(): number {
+    return Math.floor(Math.random() * GuessGame.MAX_NUMBER) + 1;
+  }
+
+public isValidGuess(guess?: number): boolean {
+  return(
+    guess !== undefined &&
+    guess >= 1 &&
+    guess <= GuessGame.MAX_NUMBER
+  )
+}
 
   submitGuess() {
-    if (this.userGuess < this.secretNumber) {
-      this.feedbackMessage = 'Too Low! Try again.'
-      this.numberOfAttempts--;
-    } else if (this.userGuess > this.secretNumber) {
-      this.feedbackMessage = 'Too High! Try again.'
-      this.numberOfAttempts--;
-    } else if (this.userGuess === this.secretNumber) {
-      this.feedbackMessage = 'Congratulations! You guessed it right!'
-    } else if (this.numberOfAttempts <= 0) {
-      this.feedbackMessage = 'Game Over! You have run out of attempts. The number was ' + this.secretNumber;
+    if (!this.isValidGuess(this.guessedNumber)) {
+      this.feedbackMessage = `Enter a number between 1 and ${GuessGame.MAX_NUMBER}`;
+      return
+    }
+    this.attemptsLeft--;
+    this.evaluateGuess();
+  }
+
+  private evaluateGuess() {
+    if(this.guessedNumber === this.secretNumber) {
+      this.endGame(true)
+    } else if (this.attemptsLeft === 0) {
+      this.endGame(false);
     } else {
-      this.feedbackMessage = 'Invalid input. Please enter a number between 1 and 100.'
+      this.feedbackMessage = this.guessedNumber! > this.secretNumber
+      ? "Too High! Try again"
+      : "Too Low! Try again";
     }
 
   }
 
+  private endGame(isWin?: boolean): void {
+    this.gameOver = true;
+    this.feedbackMessage = isWin
+    ? 'Congratulations! You guessed the correct Number'
+    : `Sorry you ran out of attempts and lost the game. The correct number was ${this.secretNumber}`
+  }
+
+  resetGame(): void {
+    this.secretNumber = this.generateRandomNumber()
+    this.attemptsLeft = GuessGame.MAZ_ATTEMPTS;
+    this.gameOver = false;
+    this.feedbackMessage = '';
+    this.guessedNumber = undefined;
+  }
 }
